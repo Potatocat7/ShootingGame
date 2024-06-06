2e3ee0632a964eb6d594ab5445e0dc16cd296320
potatocat <potatocat7@gmail.com>
2024-06-06 14:52:36 +0900

    モデル構成の追加

---
+++ Assets/Scripts/EnemyEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//エディターで呼び出せるように
+[CreateAssetMenu(fileName = "EnemyEntity", menuName = "Create EnemyEntity")]
+public class EnemyEntity : ScriptableObject
+{
+    public Sprite image;
+    public int scorePoint;
+    public BulletSelector.BulletTypeKind typeBullet;
+    public int bulletInterval;
+    public float enemySpeed;
+    public int enemyHitPoint;
+    public EnemyManager.EnemyMoveTypeKind moveType;
+    public EnemyManager.EnBullDirTypeKind bulletDirection;
+}
---
+++ Assets/Scripts/EnemyModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyModel : MonoBehaviour
+{
+    public Sprite image;
+    public int scorePoint;
+    public BulletSelector.BulletTypeKind typeBullet;
+    public int bulletInterval;
+    public float enemySpeed;
+    public int enemyHitPoint;
+    public EnemyManager.EnemyMoveTypeKind moveType;
+    public EnemyManager.EnBullDirTypeKind bulletDirection;
+
+    public EnemyModel(int num)
+    {
+        EnemyEntity enemyEntity = Resources.Load<EnemyEntity>("Enemy/Enemy_" + num.ToString());
+        image = enemyEntity.image;
+        scorePoint = enemyEntity.scorePoint;
+        typeBullet = enemyEntity.typeBullet;
+        bulletInterval = enemyEntity.bulletInterval;
+        enemySpeed = enemyEntity.enemySpeed;
+        enemyHitPoint = enemyEntity.enemyHitPoint;
+        moveType = enemyEntity.moveType;
+        bulletDirection = enemyEntity.bulletDirection;
+    }
+}
